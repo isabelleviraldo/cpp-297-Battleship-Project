359e0f79e7de7b4be1f0698954e557f7f9c2a3f1
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2023-09-21 23:40:14 -0700

    Update other.h

--- battleship-project-step3/other.h
+++ battleship-project-step3/other.h
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <tuple>
+
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
 
 using namespace std;
 
 const int ROWS = 10;
 const int COLS = 10;
 
+//initialising functions
+tuple<string, char> checkSuccess(int currentRow, int currentCol);
+
+//initialising classes
 namespace call {
         
     class gameBoard{
@@ -14,14 +22,34 @@ namespace call {
     
         public:
             gameBoard(){}
-    
             void createBoard();
-    
             void showBoard();
-    
             void setVisible(int r, int c, string newValue);
-    
             void setHidden(int r, int c, char newValue);
     };
-
+    
+    class ship{
+        private:
+            string name;
+            char symbol;
+            int length;
+            char bodyState[5] = {'.', '.', '.', '.', '.'};
+            int shipLocation[5][2] = {{-1, -1}, 
+                                      {-1, -1},
+                                      {-1, -1}, 
+                                      {-1, -1},
+                                      {-1, -1}};
+            
+        public:
+            ship(){}
+            void setShip(string n){}
+            void setLocation(int x, int r, int c){}
+            string getName(){}
+            string getSymbol(){}
+            string getLength(){}
+            tuple<char, char, char, char, char> getBodyState(){}
+            tuple<int, int, int, int, int, int, int, int, int, int> getLocation(){}
+    };
 }
+
+#endif
