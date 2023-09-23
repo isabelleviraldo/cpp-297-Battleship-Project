c2379e9386cc764c8c8c73292fc01f6b7f1b45be
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2023-09-22 22:04:49 -0700

    Update placeShipAuto_def.cpp

--- battleship-project-step3/placeShipAuto_def.cpp
+++ battleship-project-step3/placeShipAuto_def.cpp
@@ -3,45 +3,48 @@
 using namespace std;
 using namespace call;
 
-//return direction, initial (x, y)
-tuple<int, int, int> placeShipAuto(int l, gameBoard thisGame){
-    int R = ROWS - 5 + (5 - l);
-    int C = COLS - 5 + (5 - l);
+//inputs length of the ship im working with, as well as the class gameBoard im using (it has my grid info)
+//returns direction, and initial (row, col)
+tuple<int, int, int> placeShipAuto(int len, gameBoard thisGame){
+    //make sure that it wont go out of bounds of grid
+    int R = ROWS - len;
+    int C = COLS - len;
+    
     int row;
     int col;
     int direction;
     
+    //loops until a direction with (row, col) is found that doesnt hit anything
     while (true){
+        //get random direction and location
+        direction = (rand() % 2);
         row = (rand() % R);
         col = (rand() % C);
-        direction = (rand() % 2);
-        cout << row << " " << col << endl;
         
         if (direction == 0){
             //right
-            for (int i = 0; i < (l + 1); i++){
-                cout << i << endl;
+            for (int i = 0; i < (len + 1); i++){
+                //if location at point ISNT water, exit for loop to get new point
                 if (thisGame.getHidden(row, col + i) != '.'){
-                    i = (ROWS + COLS);
-                    cout << "IN HERE" << endl;
+                    i = (ROWS + COLS); 
                 }
-                if (i == l){
+                //if everything is clear, this direction and (row, col) is good to use
+                if (i == len){
                     return {direction, row, col};
                 }
             }
         } else {
             //down
-            for (int i = 0; i < (l + 1); i++){
-                cout << i << endl;
+            for (int i = 0; i < (len + 1); i++){
+                //if location at point ISNT water, exit for loop to get new point
                 if (thisGame.getHidden(row + i, col) != '.'){
                     i = (ROWS + COLS);
-                    cout << "IN HERE" << endl;
                 }
-                if (i == l){
+                //if everything is clear, this direction and (row, col) is good to use
+                if (i == len){
                     return {direction, row, col};
                 }
             }
         }
     }
 };
-
