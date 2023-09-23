ff77e8e2caced421968065e22433eb4a0bf31d6b
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2023-09-22 17:46:34 -0700

    Update other.h

--- battleship-project-step3/other.h
+++ battleship-project-step3/other.h
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <tuple>
+#include <cstring>
+#include <cstdlib>
+#include <time.h>
 
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
@@ -11,19 +14,22 @@ const int COLS = 10;
 
 //initialising functions
 tuple<string, char> checkSuccess(int currentRow, int currentCol);
+//tuple<int, int, int> placeShipAuto();
 
 //initialising classes
 namespace call {
-        
     class gameBoard{
         private:
             string visibleBoard[ROWS][COLS];
             char hiddenBoard[ROWS][COLS];
-    
+            
         public:
-            gameBoard(){}
+            gameBoard(){};
             void createBoard();
             void showBoard();
+            void showHBoard();
+            string getVisible(int r, int c);
+            char getHidden(int r, int c);
             void setVisible(int r, int c, string newValue);
             void setHidden(int r, int c, char newValue);
     };
@@ -41,14 +47,14 @@ namespace call {
                                       {-1, -1}};
             
         public:
-            ship(){}
-            void setShip(string n){}
-            void setLocation(int x, int r, int c){}
-            string getName(){}
-            string getSymbol(){}
-            string getLength(){}
-            tuple<char, char, char, char, char> getBodyState(){}
-            tuple<int, int, int, int, int, int, int, int, int, int> getLocation(){}
+            ship(){};
+            void setShip(string n);
+            void setLocation(int x, int r, int c);
+            string getName();
+            char getSymbol();
+            int getLength();
+            tuple<char, char, char, char, char> getBodyState();
+            tuple<int, int, int, int, int, int, int, int, int, int> getLocation();
     };
 }
 
