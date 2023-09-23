7f320bc8d77d8736637c2902c038867899110698
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2023-09-22 17:51:16 -0700

    Update placeShipAuto_def.cpp

--- battleship-project-step3/placeShipAuto_def.cpp
+++ battleship-project-step3/placeShipAuto_def.cpp
@@ -4,25 +4,44 @@ using namespace std;
 using namespace call;
 
 //return direction, initial (x, y)
-tuple<int, int, int> placeShipAuto(int l){
-    bool found = false;
-    int R = ROWS - (5 - l);
-    int C = COLS - (5 - l);
+tuple<int, int, int> placeShipAuto(int l, gameBoard thisGame){
+    int R = ROWS - 5 + (5 - l);
+    int C = COLS - 5 + (5 - l);
+    int row;
+    int col;
+    int direction;
     
-    while (!found){
-        int row = (rand() % ROWS);
-        int col = (rand() % COLS);
-        int direction = (rand() % 2);
+    while (true){
+        row = (rand() % R);
+        col = (rand() % C);
+        direction = (rand() % 2);
+        cout << row << " " << col << endl;
         
         if (direction == 0){
             //right
-            
-            
+            for (int i = 0; i < (l + 1); i++){
+                cout << i << endl;
+                if (thisGame.getHidden(row, col + i) != '.'){
+                    i = (ROWS + COLS);
+                    cout << "IN HERE" << endl;
+                }
+                if (i == l){
+                    return {direction, row, col};
+                }
+            }
         } else {
             //down
+            for (int i = 0; i < (l + 1); i++){
+                cout << i << endl;
+                if (thisGame.getHidden(row + i, col) != '.'){
+                    i = (ROWS + COLS);
+                    cout << "IN HERE" << endl;
+                }
+                if (i == l){
+                    return {direction, row, col};
+                }
+            }
         }
     }
-    
-    return {direction, row, col};
-    
 };
+
